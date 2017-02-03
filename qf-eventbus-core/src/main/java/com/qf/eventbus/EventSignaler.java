521c3bd5279f6cc164f63f9965dcafcd1ee4ff36
qiongqi <qiongqi@ixiaopu.com>
2017-02-03 13:19:00 +0800

    init version

--- qf-eventbus-core/src/main/java/com/qf/eventbus/EventSignaler.java
+++ qf-eventbus-core/src/main/java/com/qf/eventbus/EventSignaler.java
@@ -1,8 +1,12 @@
 package com.qf.eventbus;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 
@@ -24,44 +28,169 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class EventSignaler implements EventPublisher, EventSubscriber {
 	
-	private AbstractBusManager busManager;
+	private final ConcurrentHashMap<EventWrap, SenderBuilder> senderMap = new ConcurrentHashMap<EventWrap, SenderBuilder>();
+	private final ConcurrentHashMap<String, ReceiverBuilder> receiverMap = new ConcurrentHashMap<String, ReceiverBuilder>();
 	
-	ConcurrentHashMap<String, List<Sender>> channelMapping = new ConcurrentHashMap<String, List<Sender>>();
+	private final ConcurrentHashMap<String, ChannelHandler<? extends AbstractChannel>> handlerMapping = new ConcurrentHashMap<String, ChannelHandler<? extends AbstractChannel>>();
+	
+	private Class<? extends AbstractChannel> channelClass = DefaultChannel.class;
+	
+	private AbstractBusManager busManager;
 
 	@SuppressWarnings("unchecked")
-	@Override
 	public <T extends AbstractChannel> ChannelHandler<T> buildChannel(String name) {
-		ChannelHandler<DefaultChannel> handler = busManager.buildChannel(name, DefaultChannel.class);
+		ChannelHandler<? extends AbstractChannel> handler = busManager.buildChannel(name, channelClass);
 		if (handler != null) {
-			channelMapping.putIfAbsent(name, new ArrayList<Sender>());
+			handler = handlerMapping.putIfAbsent(name, handler);
 			handler.open();
 		}
-		return (ChannelHandler<T>)handler;
+		ChannelHandler<? extends AbstractChannel> chl = handlerMapping.get(name);
+		return chl != null ? (ChannelHandler<T>)chl : null;
 	}
 
-	@Override
-	public void register(Class<? extends Event> eventClass, String channel) {
-		busManager.bindEvent(eventClass, channel);
+	public void register(final Class<? extends Event> eventClass, final String channel) {
+		EventWrap wrap = new EventWrap(eventClass, channel);
+		if (!senderMap.containsKey(wrap)) {
+			SenderBuilder builder = new SenderBuilder(wrap);
+			SenderBuilder existed = senderMap.putIfAbsent(wrap, builder);
+			if (existed != null && builder.getBuilderId().equals(existed.getBuilderId())) {
+				existed.build();
+			}
+		}
 	}
 
-	@Override
 	public void unRegister(Class<? extends Event> eventClass, String channel) {
-		
+		EventWrap wrap = new EventWrap(eventClass, channel);
+		SenderBuilder builder = senderMap.get(wrap);
+		if (builder != null) {
+			builder.distory();
+			senderMap.remove(wrap);
+			builder = null;
+		}
 	}
 	
-	@Override
 	public void subscribe(String channel, Listener listener) {
-		
+		if (!receiverMap.containsKey(channel)) {
+			ReceiverBuilder builder = new ReceiverBuilder(channel);
+			ReceiverBuilder existed = receiverMap.putIfAbsent(channel, builder);
+			if (existed != null && builder.getBuilderId().equals(existed.getBuilderId())) {
+				existed.build();
+			}
+		}
 	}
 
-	@Override
 	public void unSubscribe(String channel) {
-		
+		ReceiverBuilder builder = receiverMap.get(channel);
+		if (builder != null) {
+			builder.distory();
+			receiverMap.remove(builder.getChannel());
+			builder = null;
+		}
 	}
 
-	@Override
 	public List<String> getSubscribedChannelList() {
-		return null;
+		return Collections.unmodifiableList(new ArrayList<String>(receiverMap.keySet()));
+	}
+	
+	private class EventWrap {
+		
+		private Class<? extends Event> eventClass;
+		private String channel;
+		
+		EventWrap(Class<? extends Event> eventClass, String channel) {
+			this.eventClass = eventClass;
+			this.channel = channel;
+		}
+		
+		String getChannel() {
+			return channel;
+		}
+		
+		Class<? extends Event> getEventClass() {
+			return eventClass;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			boolean result = false;
+			if (eventClass != null && StringUtils.isNotBlank(channel) && (obj instanceof EventWrap)) {
+				EventWrap wrap = (EventWrap)obj;
+				result = (eventClass == wrap.getEventClass() && channel.equals(wrap.getChannel()));
+			}
+			return result;
+		}
+	}
+	
+	private class SenderBuilder {
+		
+		private final String builderId = UUID.randomUUID().toString().replace("-", "");
+		
+		private Sender sender;
+		private EventWrap eventWrap;
+		
+		SenderBuilder(EventWrap wrap) {
+			this.eventWrap = wrap;
+		}
+		
+		public String getBuilderId() {
+			return builderId;
+		}
+
+		Sender getSender() {
+			return sender;
+		}
+		
+		EventWrap getEventWrap() {
+			return eventWrap;
+		}
+		
+		void build() {
+			if (sender == null && eventWrap != null) {
+				sender = busManager.bindEvent(eventWrap.getEventClass(), eventWrap.getChannel());
+			}
+		}
+		
+		void distory() {
+			if (sender != null) {
+				busManager.unbindEvent(sender.getId(), eventWrap.getChannel());
+			}
+		}
+	}
+	
+	private class ReceiverBuilder {
+		
+		private final String builderId = UUID.randomUUID().toString().replace("-", "");
+		
+		private Receiver receiver;
+		private String channel;
+		
+		ReceiverBuilder(String channel) {
+			this.channel = channel;
+		}
+		
+		public String getBuilderId() {
+			return builderId;
+		}
+
+		public Receiver getReceiver() {
+			return receiver;
+		}
+		
+		public String getChannel() {
+			return channel;
+		}
+		
+		void build() {
+			if (receiver == null && StringUtils.isNotBlank(channel)) {
+				receiver = busManager.subscribe(channel);
+			}
+		}
+		
+		void distory() {
+			if (receiver != null) {
+				busManager.unSubscribe(receiver.getId(), channel);
+			}
+		}
 	}
 
 }
