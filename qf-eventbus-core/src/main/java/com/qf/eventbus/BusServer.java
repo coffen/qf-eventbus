dc83818356a9f215e990807521b2878843bc63a4
qiongqi <qiongqi@ixiaopu.com>
2017-02-06 22:30:04 +0800

    开发完成

--- qf-eventbus-core/src/main/java/com/qf/eventbus/BusServer.java
+++ qf-eventbus-core/src/main/java/com/qf/eventbus/BusServer.java
@@ -7,7 +7,7 @@ import java.lang.reflect.Proxy;
 /**
  * 
  * <p>
- * Project Name: 买到手抽筋
+ * Project Name: C2C商城
  * <br>
  * Description: 总线服务器
  * <br>
@@ -18,22 +18,22 @@ import java.lang.reflect.Proxy;
  * Company: 杭州偶尔科技有限公司
  * <br>
  * @author 穷奇
- * @create time：2017-02-06 22:14:52 
+ * @create time：2017年2月6日 下午22:14:52
  * @version: v1.0
  *
  */
 public class BusServer {
 	
-	private BusManager manager = new DefaultBusManager();
+	private static BusManager manager = new DefaultBusManager();
 	
-	public BusSignaler buildSignaler() {
+	public static BusSignaler buildSignaler() {
 		BusManager proxy = buildBusManagerProxy();
 		BusSignaler signaler = new BusSignaler(proxy);
 		return signaler;
 	}
 	
-	private BusManager buildBusManagerProxy() {
-		return (BusManager)Proxy.newProxyInstance(getClass().getClassLoader(), new Class<?>[] { BusManager.class }, new InvocationHandler() {
+	private static BusManager buildBusManagerProxy() {
+		return (BusManager)Proxy.newProxyInstance(System.class.getClassLoader(), new Class<?>[] { BusManager.class }, new InvocationHandler() {
 			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 				return method.invoke(manager, args);
 			}
