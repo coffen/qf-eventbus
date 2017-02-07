219735182dfab42c64c251505c0762342508e0c2
qiongqi <qiongqi@ixiaopu.com>
2017-02-07 17:27:08 +0800

    修改

--- qf-eventbus-core/src/main/java/com/qf/eventbus/exception/SignalerClosedException.java
+++ qf-eventbus-core/src/main/java/com/qf/eventbus/exception/SignalerClosedException.java
@@ -1,5 +1,23 @@
 package com.qf.eventbus.exception;
 
+/**
+ * 
+ * <p>
+ * Project Name: C2C商城
+ * <br>
+ * Description: BusSignaler已关闭异常
+ * <br>
+ * File Name: SignalerClosedException.java
+ * <br>
+ * Copyright: Copyright (C) 2015 All Rights Reserved.
+ * <br>
+ * Company: 杭州偶尔科技有限公司
+ * <br>
+ * @author 穷奇
+ * @create time：2017年2月7日 下午5:26:33 
+ * @version: v1.0
+ *
+ */
 public class SignalerClosedException extends Exception {
 
 	private static final long serialVersionUID = 4971851119803796435L;
